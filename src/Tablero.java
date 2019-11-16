1ee7c258b3c4190fb681960a42b4985267ae7988
Joan Busquets Armengol <joan.busquetsa@e-campus.uab.cat>
2019-11-16 13:08:49 +0100

    TableroTest-Colocarbarco
    Test mètodo ColocarBarco, en tablero solo colocaremos Barco y haremos una clase Comprovación para comprobar que donde se coloca és correcto.

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,4 +1,19 @@
 
 public class Tablero {
-
+	private String[][] tablero;
+	private static final int numeroFilas = 10;
+	private static final int numeroColumnas = 10;
+	private static final String agua = "  ";
+	
+	public Tablero() {        
+        tablero = new String[numeroFilas][numeroColumnas];
+        for(int i = 0; i < numeroFilas; i++) {
+        	for(int j = 0; j < numeroColumnas; j++) {
+        		tablero[i][j] = agua;
+        	}
+        }
+	}
+	String[][] getTablero(){
+		return tablero;
+	}
 }
--- src/TableroTest.java
+++ src/TableroTest.java
@@ -1,12 +1,59 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class TableroTest {
 
+	Tablero t = new Tablero();
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testCrearTablero() {
+		int numFilas=10;
+		int numColumnas=10;
+		String[][] tableroBarcosEsperado = {
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}
+		};
+		String[][] tableroBarcos=t.getTablero();
+		for(int i=0;i<numFilas;i++) {
+			for(int j=0;j<numColumnas;j++) {
+				assertEquals(tableroBarcos[i][j],tableroBarcosEsperado[i][j]);
+			}
+		}
+	}
+	@Test
+	public void testColocarBarco() {
+		String[][] tableroBarcosEsperado = {
+				{"B1", "B1", "B1", "B1", "B1", "  ", "  ", "  ", "  ", "B4"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B4"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B4"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B4"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B4"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B4"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B4"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B3"},
+				{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "B3"},
+				{"B2", "B2", "B2", "B2", "B2", "  ", "  ", "  ", "  ", "B3"}
+		};
+		t.colocarBarco(0, 0, 'r', 5, "B1");
+		t.colocarBarco(9, 4, 'l', 5, "B2");
+		t.colocarBarco(9, 9, 'u', 3, "B3");
+		t.colocarBarco(0, 9, 'd', 7, "B4");
+		String[][] tableroBarcos=t.getTablero();
+		for(int i=0;i<numFilas;i++) {
+			for(int j=0;j<numColumnas;j++) {
+				assertEquals(tableroBarcos[i][j],tableroBarcosEsperado[i][j]);
+			}
+		}
 	}
 
 }
