39a6212d566eee44901342a4a02111852f8c0156
Joan Busquets Armengol <joan.busquetsa@e-campus.uab.cat>
2019-11-17 09:56:24 +0100

    Comprobacion-Implementación
    Implementación primeros procesos comprobación i más test para otras funciones.

--- src/Comprobacion.java
+++ src/Comprobacion.java
@@ -1,4 +1,30 @@
 
 public class Comprobacion {
+	
+	private static int MAXFILAS=10;
+	private static int MAXCOLUMNAS=10;
+	
+	Comprobacion(){
+	}
+	
+	static boolean isFilaCorrecta(int fila) {
+		boolean filaCorrecta = false;
+		
+		if(fila >= 0 && fila < MAXFILAS) {
+			filaCorrecta = true;
+		}
+		
+		return filaCorrecta;
+	}
+	
+	static boolean isColumnaCorrecta(int columna) {
+		boolean columnaCorrecta = false;
+		
+		if(columna >= 0 && columna < MAXCOLUMNAS) {
+			columnaCorrecta = true;
+		}
+		
+		return columnaCorrecta;
+	}
 
 }
--- src/ComprobacionTest.java
+++ src/ComprobacionTest.java
@@ -35,4 +35,62 @@ class ComprobacionTest {
 		boolean r5 = comp.isColumnaCorrecta(10);
 		assertEquals(r5,false);
 	}
+	@Test
+	public void testCoordenadaCorrecta() {
+		boolean r1 = comp.isCoordenadaCorrecta(-1, 1);
+		assertEquals(r1,false);
+		boolean r2 = comp.isCoordenadaCorrecta(1, -1);
+		assertEquals(r2,false);
+		boolean r3 = comp.isCoordenadaCorrecta(5, 5);
+		assertEquals(r3,true);
+		boolean r4 = comp.isCoordenadaCorrecta(10, -1);
+		assertEquals(r4,false);
+	}
+	@Test
+	public void testOrientacionCorrecta() {
+		Tablero t=new Tablero();
+		//Limits i frontera taulell
+		boolean r1 = comp.isOrientacionCorrecta(t, 0, 0, 'u', 5);
+		assertEquals(r1,false);
+		boolean r2 = comp.isOrientacionCorrecta(t, 0, 0, 'd', 3);
+		assertEquals(r2,true);
+		boolean r3 = comp.isOrientacionCorrecta(t, 0, 0, 'l', 2);
+		assertEquals(r3,false);
+		boolean r4 = comp.isOrientacionCorrecta(t, 0, 0, 'r', 3);
+		assertEquals(r4,true);
+		boolean r11 = comp.isOrientacionCorrecta(t, 9, 0, 'u', 5);
+		assertEquals(r11,true);
+		boolean r12 = comp.isOrientacionCorrecta(t, 9, 0, 'd', 3);
+		assertEquals(r12,false);
+		boolean r13 = comp.isOrientacionCorrecta(t, 9, 0, 'l', 2);
+		assertEquals(r13,false);
+		boolean r14 = comp.isOrientacionCorrecta(t, 9, 0, 'r', 3);
+		assertEquals(r14,true);
+		boolean r21 = comp.isOrientacionCorrecta(t, 0, 9, 'u', 5);
+		assertEquals(r21,false);
+		boolean r22 = comp.isOrientacionCorrecta(t, 0, 9, 'd', 3);
+		assertEquals(r22,true);
+		boolean r23 = comp.isOrientacionCorrecta(t, 0, 9, 'l', 2);
+		assertEquals(r23,true);
+		boolean r24 = comp.isOrientacionCorrecta(t, 0, 9, 'r', 3);
+		assertEquals(r24,false);
+		boolean r31 = comp.isOrientacionCorrecta(t, 9, 9, 'u', 5);
+		assertEquals(r31,true);
+		boolean r32 = comp.isOrientacionCorrecta(t, 9, 9, 'd', 3);
+		assertEquals(r32,false);
+		boolean r33 = comp.isOrientacionCorrecta(t, 9, 9, 'l', 2);
+		assertEquals(r33,true);
+		boolean r34 = comp.isOrientacionCorrecta(t, 9, 9, 'r', 3);
+		assertEquals(r34,false);
+		
+		//Valors no valids
+		boolean r5 = comp.isOrientacionCorrecta(t, 0, 0, 'z', 5);
+		assertEquals(r5,false);
+		boolean r6 = comp.isOrientacionCorrecta(t, 0, 0, 'd', -1);
+		assertEquals(r6,false);
+		boolean r7 = comp.isOrientacionCorrecta(t, 0, 0, 'l', 10);
+		assertEquals(r7,false);
+		boolean r8 = comp.isOrientacionCorrecta(t, -11, -1, 'r', 5);
+		assertEquals(r8,false);
+	}
 }
