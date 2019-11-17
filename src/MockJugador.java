f3ee1b6f16a9889e0227a5278daa259f39c4be08
Joan Busquets Armengol <joan.busquetsa@e-campus.uab.cat>
2019-11-17 13:18:14 +0100

    MockJugador y TestJugador-Test para classe Partida
    Creación de un mock para poder testear la classe Partida sin tener acabada la classe Jugador

---
+++ src/MockJugador.java
@@ -0,0 +1,41 @@
+
+public class MockJugador extends Jugador{
+		
+	private int id;
+	private int vida=5;
+	private Tablero t;
+	
+	public MockJugador(int idMock){
+		id=idMock;
+		t=new Tablero();
+	}
+
+	@Override
+	public boolean colocarBarcos() {
+		return true;
+	}
+
+	@Override
+	public void setCoordenadas(Coordenadas c) {}
+
+
+	public void disparar(MockJugador jugadorEnemigo) {
+		int v=jugadorEnemigo.getVida();
+		v--;
+		jugadorEnemigo.setVida(v);
+	}
+
+	@Override
+	public int getVida() {
+		return vida;
+	}
+
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	public void setVida(int v) {
+		vida=v;
+	}
+}
---
+++ src/PartidaTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PartidaTest {
+
+	Partida p = new Partida('1');
+	Jugador j1 = new MockJugador(1);
+	Jugador j2 = new MockJugador(2);
+	
+	@Test
+	public void testEmpezar() {
+		p.setJugadors(j1, j2);
+		p.empezar();
+		assertEquals(j1.getVida(),1);
+		assertEquals(j2.getVida(),0);
+	}
+}
