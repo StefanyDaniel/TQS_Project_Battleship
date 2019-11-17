e37f1ce2c30c02cbaa100f6520860c97cf70f557
Joan Busquets Armengol <joan.busquetsa@e-campus.uab.cat>
2019-11-17 21:02:35 +0100

    Update Tablero.java
    Implementacion MarcarImpacto y Hundir

--- src/Tablero.java
+++ src/Tablero.java
@@ -65,4 +65,47 @@ public class Tablero {
 			System.out.println("----------------------------------");
 		}
 	}
+	
+	public boolean marcarImpacto(Tablero configuracionBarcosEnemigo, Barco[] barcosEnemigo, int fila, int columna) {
+		boolean impactoMarcado = false;		
+		
+		if(Comprobacion.isFilaCorrecta(fila)) {
+			if(Comprobacion.isColumnaCorrecta(columna)) {
+				if(tablero[fila][columna] == agua) {
+					if(configuracionBarcosEnemigo.getTablero()[fila][columna] == agua) {
+						tablero[fila][columna] = " O";
+						impactoMarcado = true;
+					}
+					else {
+						tablero[fila][columna] = " T";
+						String alias = configuracionBarcosEnemigo.getTablero()[fila][columna];
+						for(Barco b : barcosEnemigo) {
+							if(b.getAlias().equals(alias)) {
+								b.actualizarVida();
+								if(b.isHundido()) {
+									hundir(configuracionBarcosEnemigo,b);
+								}
+							}
+						}
+						impactoMarcado = true;
+					}
+				}
+				else {
+					System.out.println("Ya has disparado en esta posicion!");
+				}
+			}
+		}
+		return impactoMarcado;
+	}
+	
+	public void hundir(Tablero configuracionBarcosEnemigo, Barco bHundido) {
+		String[][] t = configuracionBarcosEnemigo.getTablero();
+		for(int fila=0; fila<numeroFilas; fila++) {
+			for(int columna=0; columna<numeroColumnas; columna++) {
+				if(t[fila][columna].equals(bHundido.getAlias())) {
+					tablero[fila][columna] = " H";
+				}
+			}
+		}
+	}
 }
